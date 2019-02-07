4d79ba5e277117399a04838a347cc88414b2b5c6
igor-str <igorok.strilchuk@gmail.com>
2019-02-07 22:38:41 +0200

    finished part 1

--- server/src/index.js
+++ server/src/index.js
@@ -11,3 +11,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(process.env.PORT || config.port, () => console.log(`Server start on port ${config.port} ...`));
+
+app.get('/posts', (req, res) => {
+    res.send(
+        [{
+            title: "Hello World!",
+            description: "Hi there! How are you?"
+        }]
+    )
+})
