e13c0ab049d12e0de88e2b2351d38fc7a1f9ff9b
igor-str <igorok.strilchuk@gmail.com>
2019-02-20 19:39:49 +0200

    update server

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,25 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const config = require('./config/config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(require('./routes/posts'));
+
+mongoose.connect(config.dbURL, config.dbOptions);
+mongoose.connection
+  .once('open', () => {
+    console.log(`Mongoose - successful connection ...`);
+    app.listen(process.env.PORT || config.port,
+      () => console.log(`Server start on port ${config.port} ...`));
+  })
+  .on('error', error => console.warn(error));
 
 app.listen(process.env.PORT || config.port, () => console.log(`Server start on port ${config.port} ...`));
 
@@ -19,4 +32,4 @@ app.get('/posts', (req, res) => {
             description: "Hi there! How are you?"
         }]
     )
-})
+});
