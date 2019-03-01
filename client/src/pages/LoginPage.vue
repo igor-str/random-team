8e9b6ab060184b56d7cc7a6c4da5cf25761f1048
igor-str <igorok.strilchuk@gmail.com>
2019-03-01 18:12:40 +0200

    created login page

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="auth-page">
+  <div class="login-page">
     <div class="container">
-      <h1>Registration</h1>
+      <h1>Log in</h1>
       <div class="form col-md-6 offset-md-3">
-        <div class="form-group">
-          <input type="text" name="firstName" id="firstName"
-                 placeholder="First name" class="form-control"
-                 v-model.trim="user.firstName">
-        </div>
-        <div class="form-group">
-          <input type="text" name="lastName" id="lastName"
-                 placeholder="Last name" class="form-control"
-                 v-model.trim="user.lastName">
-        </div>
         <div class="form-group">
           <input type="email" name="email" id="email"
                  placeholder="Email" class="form-control"
@@ -23,12 +13,6 @@
                  placeholder="Enter password" class="form-control"
                  v-model.trim="user.password">
         </div>
-        <div class="form-group">
-          <input type="password" name="confirm-password" id="confirm-password"
-                 placeholder="Confirm password" class="form-control"
-                 v-model.trim="user.confirmPassword">
-        </div>
-        <switch-button v-model="user.isAdmin" title="Do you want create own team?"/>
         <div v-if="errors" v-for="(item, index) in errors" :key="index">
           <div class="alert alert-danger" role="alert">
             {{item}}
@@ -36,7 +20,7 @@
         </div>
         <div class="form-group">
           <button type="button" name="addPost" id="addPost"
-                  class="btn btn-success" @blur="clearErrors()" @click="addUser()">
+                  class="btn btn-success" @blur="clearErrors()" @click="loginUser()">
             Register
           </button>
         </div>
@@ -47,35 +31,25 @@
 
 <script>
 import UsersService from '@/services/UsersService';
-import SwitchButton from '@/components/blocks/SwitchButton';
 
 export default {
-  name: 'AuthPage',
-  components: {
-    SwitchButton,
-  },
+  name: 'LoginPage',
   data() {
     return {
       user: {
-        firstName: '',
-        lastName: '',
         email: '',
         password: '',
-        confirmPassword: '',
-        isAdmin: false,
       },
       errors: [],
     };
   },
   methods: {
-    async addUser() {
-      if (this.validateBeforeAddUser()) {
+    async loginUser() {
+      if (this.validateBeforeLogin()) {
+        // TODO: create login Service
         await UsersService.addNewUser({
-          firstName: this.user.firstName,
-          lastName: this.user.lastName,
           email: this.user.email,
           password: this.user.password,
-          isAdmin: this.user.isAdmin,
         });
         // TODO: set url or name component
         // this.$router.push({ name: 'Users' });
@@ -83,11 +57,9 @@ export default {
         this.errors.push('Ooops, not the correct data! try again...');
       }
     },
-    validateBeforeAddUser() {
-      if (!this.user.firstName) return false;
-      if (!this.user.lastName) return false;
+    validateBeforeLogin() {
       if (!this.user.email) return false;
-      if (!this.user.password || this.user.password !== this.user.confirmPassword) return false;
+      if (!this.user.password) return false;
 
       return true;
     },
