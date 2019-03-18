156a0248e8beb99531291159a3b3fe1224430e8d
igor-str <igorok.strilchuk@gmail.com>
2019-03-18 14:55:32 +0200

    temp server

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
 
-router.post('/users', (req, res) => {
+router.post('/register', (req, res) => {
   const user = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -21,59 +21,6 @@ router.post('/users', (req, res) => {
         success: true,
         message: `User with ID_${data._id} saved successfully!`,
       })
-    }
-  });
-});
-
-router.get('/users', (req, res) => {
-  User.find({}, 'firstName lastName isAdmin', (err, users) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.send({ users: users });
-    }
-  }).sort({ _id: -1 });
-});
-
-router.get('/users/:id', (req, res) => {
-  User.findById(req.params.id, 'name selfRating', (err, user) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.send(user);
-    }
-  });
-});
-
-router.put('/users/:id', (req, res) => {
-  User.findById(req.params.id, 'name selfRating', (err, user) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (req.body.name) {
-        user.name = req.body.name;
-      }
-      if (req.body.selfRating) {
-        user.selfRating = req.body.selfRating;
-      }
-
-      user.save(err => {
-        if (err) {
-          res.sendStatus(500);
-        } else {
-          res.sendStatus(200);
-        }
-      });
-    }
-  });
-});
-
-router.delete('/users/:id', (req, res) => {
-  User.remove({_id: req.params.id}, err => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
     }
   });
 });
